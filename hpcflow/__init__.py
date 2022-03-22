e553b3b0f49b78c2b7bad535a24d57ab44a4eb33
aplowman <adam.jp@live.co.uk>
2022-03-22 11:42:22 +0000

    feat: pass version to RunTimeInfo

--- hpcflow/__init__.py
+++ hpcflow/__init__.py
@@ -3,6 +3,6 @@ from hpcflow.app_log import AppLog
 from hpcflow.runtime import RunTimeInfo
 
 log = AppLog(__name__)
-RUN_TIME_INFO = RunTimeInfo(__name__)
+RUN_TIME_INFO = RunTimeInfo(__name__, __version__)
 
 from hpcflow.hpcflow import HPCFlowApp
