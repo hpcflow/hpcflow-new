9695dd207e8c43b4c8361d02043ce1a4432d9b77
aplowman <adam.jp@live.co.uk>
2022-10-29 18:25:21 +0100

    build: add back sdk data hiddenimport!

--- pyinstaller/hook-hpcflow.py
+++ pyinstaller/hook-hpcflow.py
@@ -1,5 +1,7 @@
 from PyInstaller.utils.hooks import collect_data_files
 
+hiddenimports = ["hpcflow.sdk.data"]
+
 datas = collect_data_files("hpcflow.sdk.data")
 datas += collect_data_files(
     "hpcflow.tests",
