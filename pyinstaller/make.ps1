7aa47e3471b1dd94e184a66e39858e10318190f9
cjfullerton <chris.j.fullerton@googlemail.com>
2022-11-07 11:04:20 +0000

    fix: Change onefolder to onefir in makefiles

--- pyinstaller/make.ps1
+++ pyinstaller/make.ps1
@@ -7,4 +7,4 @@
 # 
 param($ExeName = "hpcflow", $LogLevel = "INFO")
 poetry run pyinstaller --log-level=$LogLevel --distpath ./dist/onefile --onefile --clean -y --name=$ExeName ..\hpcflow\cli\cli.py
-poetry run pyinstaller --log-level=$LogLevel --distpath ./dist/onedir --onefolder --clean -y --name=$ExeName ..\hpcflow\cli\cli.py
+poetry run pyinstaller --log-level=$LogLevel --distpath ./dist/onedir --onedir --clean -y --name=$ExeName ..\hpcflow\cli\cli.py
