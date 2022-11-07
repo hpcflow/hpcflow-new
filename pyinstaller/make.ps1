2f310cc195afc064ad1d99d81ae836447593cf59
cjfullerton <chris.j.fullerton@googlemail.com>
2022-11-07 10:50:59 +0000

    build: Add sep folder for onefile, onefolder build

--- pyinstaller/make.ps1
+++ pyinstaller/make.ps1
@@ -6,5 +6,5 @@
 # Might need to disable desktop cloud sync. engines during this!
 # 
 param($ExeName = "hpcflow", $LogLevel = "INFO")
-poetry run pyinstaller --log-level=$LogLevel --onefile --clean -y --name=$ExeName ..\hpcflow\cli\cli.py
-poetry run pyinstaller --log-level=$LogLevel --onefolder --clean -y --name=$ExeName ..\hpcflow\cli\cli.py
+poetry run pyinstaller --log-level=$LogLevel --distpath ./dist/onefile --onefile --clean -y --name=$ExeName ..\hpcflow\cli\cli.py
+poetry run pyinstaller --log-level=$LogLevel --distpath ./dist/onefolder --onefolder --clean -y --name=$ExeName ..\hpcflow\cli\cli.py
--- pyinstaller/make.sh
+++ pyinstaller/make.sh
@@ -9,5 +9,5 @@ EXE_NAME_DEFAULT="hpcflow"
 LOG_LEVEL_DEFAULT="INFO"
 EXE_NAME="${1:-$EXE_NAME_DEFAULT}"
 LOG_LEVEL="${2:-$LOG_LEVEL_DEFAULT}"
-poetry run pyinstaller --log-level=$LOG_LEVEL --onefile --clean -y --name=$EXE_NAME ../hpcflow/cli/cli.py
-poetry run pyinstaller --log-level=$LOG_LEVEL --onefolder --clean -y --name=$EXE_NAME ../hpcflow/cli/cli.py
+poetry run pyinstaller --log-level=$LOG_LEVEL --distpath ./dist/onefile --onefile --clean -y --name=$EXE_NAME ../hpcflow/cli/cli.py
+poetry run pyinstaller --log-level=$LOG_LEVEL --onefolder ./dist/onefolder --clean -y --name=$EXE_NAME ../hpcflow/cli/cli.py
