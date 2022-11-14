d53feec9d3dcd51287cf7321836bf33f8d2c0794
cjfullerton <chris.j.fullerton@googlemail.com>
2022-11-14 11:09:42 +0000

    build: Update make scripts.
    Pass pyinstaller build type option directly as command line variable to
    avoid having to use statement.

--- pyinstaller/make.ps1
+++ pyinstaller/make.ps1
@@ -6,16 +6,4 @@
 # Might need to disable desktop cloud sync. engines during this!
 # 
 param($ExeName = "hpcflow", $LogLevel = "INFO", $BuildType = 'onefile')
-if ($BuildType -eq 'onefile')
-{
-    poetry run pyinstaller --log-level=$LogLevel --distpath ./dist/onefile --onefile --clean -y --name=$ExeName ..\hpcflow\cli\cli.py
-}
-elseif ($BuildType -eq 'onefolder')
-{
-    poetry run pyinstaller --log-level=$LogLevel --distpath ./dist/onedir --onedir --clean -y --name=$ExeName ..\hpcflow\cli\cli.py
-}
-else
-{
-    Write-Warning "Build type $BuildType unknown. Specify either onefile or onefolder"
-}
-
+poetry run pyinstaller --log-level=$LogLevel --distpath ./dist/onefile --$BuildType --clean -y --name=$ExeName ..\hpcflow\cli\cli.py
--- pyinstaller/make.sh
+++ pyinstaller/make.sh
@@ -12,10 +12,4 @@ EXE_NAME="${1:-$EXE_NAME_DEFAULT}"
 LOG_LEVEL="${2:-$LOG_LEVEL_DEFAULT}"
 BUILD_TYPE="${3:-$BUILD_TYPE_DEFAULT}"
 
-if [ $BUILD_TYPE = 'onefile' ]; then
-    poetry run pyinstaller --log-level=$LOG_LEVEL --distpath ./dist/onefile --onefile --clean -y --name=$EXE_NAME ../hpcflow/cli/cli.py
-elif [ $BUILD_TYPE = 'onefolder' ]; then
-    poetry run pyinstaller --log-level=$LOG_LEVEL --distpath ./dist/onedir --onedir --clean -y --name=$EXE_NAME ../hpcflow/cli/cli.py
-else
-    echo 'Error: Build tyope ${BUILD_TYPE} unknown. Specify either onefile or onefolder.'
-fi
+poetry run pyinstaller --log-level=$LOG_LEVEL --distpath ./dist/onefile --$BUILD_TYPE --clean -y --name=$EXE_NAME ../hpcflow/cli/cli.py
