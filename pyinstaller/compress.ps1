63a5fd2479e4b87853410f37a1aa89df8c628910
cjfullerton <chris.j.fullerton@googlemail.com>
2022-12-08 13:32:42 +0000

    build: Add compress script (bash)

--- pyinstaller/compress.ps1
+++ pyinstaller/compress.ps1
@@ -1,9 +1,2 @@
-# Make sure we ran one of these:
-#   `poetry install`
-#   `poetry install --without dev` (for building with pyinstaller and pytest)
-#   `poetry install --without dev,test` (for building with pyinstaller)
-# 
-# Might need to disable desktop cloud sync. engines during this!
-# 
 param($ExeName = "hpcflow", $BuildType = 'onedir')
-Compress-Archive -Path ./dist/$BuildType/$ExeName -DestinationPath ./dist/$BuildType/test_compress
+Compress-Archive -Path ./dist/$BuildType/$ExeName -DestinationPath ./dist/$BuildType/$ExeName
