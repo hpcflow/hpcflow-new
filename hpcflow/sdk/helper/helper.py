a4cf6bbdfc3661a61f1f87191461d2bc05991f15
hpcflow-actions <hpcflow-actions@users.noreply.github.com>
2023-02-13 15:53:45 +0000

    pre-commit fixes [skip ci]

--- hpcflow/sdk/helper/helper.py
+++ hpcflow/sdk/helper/helper.py
@@ -368,9 +368,11 @@ def run_helper(
     helper_args = read_helper_args(app)
     try:
         while True:
-            logger.info(f"fhadb - I am inside the while True loop.\n"+
-                        f"Time left: {time_left_s}"+
-                        f"Timeout: {timeout}")
+            logger.info(
+                f"fhadb - I am inside the while True loop.\n"
+                + f"Time left: {time_left_s}"
+                + f"Timeout: {timeout}"
+            )
             time_left_s = (end_time - datetime.now()).total_seconds()
             if time_left_s <= 0:
                 helper_timeout(app, timeout, controller, logger)
