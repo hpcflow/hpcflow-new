51ebb40426e82302e34a6e1b58bd92a5815ea25b
aplowman <adam.jp@live.co.uk>
2023-08-16 13:13:23 +0100

    test: add slurm test

--- hpcflow/tests/conftest.py
+++ hpcflow/tests/conftest.py
@@ -2,8 +2,28 @@ import pytest
 import hpcflow.app as hf
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--slurm",
+        action="store_true",
+        default=False,
+        help="run slurm tests",
+    )
+
+
 def pytest_configure(config):
+    config.addinivalue_line("markers", "slurm: mark test as slurm to run")
     hf.run_time_info.in_pytest = True
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--slurm"):
+        # --slurm given in cli: do not skip slurm tests
+        return
+    skip_slurm = pytest.mark.skip(reason="need --slurm option to run")
+    for item in items:
+        if "slurm" in item.keywords:
+            item.add_marker(skip_slurm)
 
 
 def pytest_unconfigure(config):
