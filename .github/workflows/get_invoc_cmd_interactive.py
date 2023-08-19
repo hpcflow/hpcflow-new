47bdafc2f5ee478eb90cb09a6bb2f0a27de99d10
aplowman <adam.jp@live.co.uk>
2023-08-19 13:38:15 +0100

    fix(GHA): testing

---
+++ .github/workflows/get_invoc_cmd_interactive.py
@@ -0,0 +1,23 @@
+"""Script to output the invocation command within an interactive python or ipython shell.
+
+This is used in the test GHA workflow to check that we get the expected result. This only
+works on POSIX operating systems.
+
+"""
+
+import sys
+import pexpect
+
+executable = sys.argv[1]
+prompt = {
+    "python": ">>>",
+    "ipython": "In.*",
+}[executable]
+
+c = pexpect.spawnu(executable)
+c.expect(prompt)
+c.sendline("import hpcflow.app as hf")
+c.sendline("hf.run_time_info.invocation_command")
+c.expect("\('.*'\)")
+c.kill(1)
+print(c.after)
