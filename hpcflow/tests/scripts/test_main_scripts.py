d78f8b1878e3503ed162073f2324f8ceab28dc18
aplowman <adam.jp@live.co.uk>
2023-11-03 18:02:50 +0000

    test: investigate GHA test win failure

--- hpcflow/tests/scripts/test_main_scripts.py
+++ hpcflow/tests/scripts/test_main_scripts.py
@@ -39,17 +39,17 @@ def test_script_direct_in_direct_out_UNIT(null_config, tmp_path):
         print(fp.read())
 
     print(f"js_0_stdout.log:")
-    with sub.path.joinpath("js_0_stdout.log").open("rt") as fp:
+    with Path(sub.path).joinpath("js_0_stdout.log").open("rt") as fp:
         print(fp.read())
 
     print(f"js_0_stderr.log:")
-    with sub.path.joinpath("js_0_stderr.log").open("rt") as fp:
+    with Path(sub.path).joinpath("js_0_stderr.log").open("rt") as fp:
         print(fp.read())
 
     print(f"hpcflow_std.txt:")
-    with wk.path.joinpath("execute", "task_0_t1", "e_0", "r_0", "hpcflow_std.txt").open(
-        "rt"
-    ) as fp:
+    with Path(wk.path).joinpath(
+        "execute", "task_0_t1", "e_0", "r_0", "hpcflow_std.txt"
+    ).open("rt") as fp:
         print(fp.read())
 
     wk.wait()
@@ -90,17 +90,17 @@ def test_script_direct_in_direct_out(null_config, tmp_path):
         print(fp.read())
 
     print(f"js_0_stdout.log:")
-    with sub.path.joinpath("js_0_stdout.log").open("rt") as fp:
+    with Path(sub.path).joinpath("js_0_stdout.log").open("rt") as fp:
         print(fp.read())
 
     print(f"js_0_stderr.log:")
-    with sub.path.joinpath("js_0_stderr.log").open("rt") as fp:
+    with Path(sub.path).joinpath("js_0_stderr.log").open("rt") as fp:
         print(fp.read())
 
     print(f"hpcflow_std.txt:")
-    with wk.path.joinpath("execute", "task_0_t1", "e_0", "r_0", "hpcflow_std.txt").open(
-        "rt"
-    ) as fp:
+    with Path(wk.path).joinpath(
+        "execute", "task_0_t1", "e_0", "r_0", "hpcflow_std.txt"
+    ).open("rt") as fp:
         print(fp.read())
 
     wk.wait()
