0bcf7a8d6528e1fcfde7c6cc9117ccdfb752690c
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2024-05-31 12:06:24 +0100

    commands.py now typechecks

--- hpcflow/sdk/core/commands.py
+++ hpcflow/sdk/core/commands.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from functools import partial
 from pathlib import Path
 import re
-from typing import ClassVar, TYPE_CHECKING
+from typing import Any, ClassVar, TYPE_CHECKING
 
 import numpy as np
 
@@ -12,7 +12,7 @@ from hpcflow.sdk.core.errors import NoCLIFormatMethodError
 from hpcflow.sdk.core.json_like import ChildObjectSpec, JSONLike
 from hpcflow.sdk.core.parameters import ParameterValue
 if TYPE_CHECKING:
-    from collections.abc import Iterable
+    from collections.abc import Callable, Iterable
     from ..app import BaseApp
     from .actions import ActionRule
 
@@ -66,7 +66,7 @@ class Command(JSONLike):
         else:
             return self.executable or ""
 
-    def get_command_line(self, EAR, shell, env) -> tuple[str, list[tuple[str, str]]]:
+    def get_command_line(self, EAR, shell, env) -> tuple[str, list[tuple[str, ...]]]:
         """Return the resolved command line.
 
         This is ordinarily called at run-time by `Workflow.write_commands`.
@@ -191,7 +191,7 @@ class Command(JSONLike):
                 string=cmd_str,
             )
 
-        shell_vars = []
+        shell_vars: list[tuple[str, ...]] = []
         out_types = self.get_output_types()
         if out_types["stdout"]:
             # TODO: also map stderr/both if possible
@@ -235,7 +235,7 @@ class Command(JSONLike):
 
     @staticmethod
     def _prepare_kwargs_from_string(args_str: str | None, doubled_quoted_args=None):
-        kwargs = {}
+        kwargs: dict[str, str] = {}
         if args_str is None:
             return kwargs
 
@@ -257,16 +257,16 @@ class Command(JSONLike):
                 kwargs[name_i] = value
         return kwargs
 
-    def process_std_stream(self, name: str, value: str, stderr: bool):
-        def _parse_list(lst_str: str, item_type: str = "str", delim: str = " "):
+    def process_std_stream(self, name: str, value: str, stderr: bool) -> Any:
+        def _parse_list(lst_str: str, item_type: str = "str", delim: str = " ") -> list[Any]:
             return [parse_types[item_type](i) for i in lst_str.split(delim)]
 
-        def _parse_array(arr_str: str, item_type: str = "float", delim: str = " "):
+        def _parse_array(arr_str: str, item_type: str = "float", delim: str = " ") -> np.ndarray[Any, np.dtype[Any]]:
             return np.array(
                 _parse_list(lst_str=arr_str, item_type=item_type, delim=delim)
             )
 
-        def _parse_bool(bool_str):
+        def _parse_bool(bool_str: str) -> bool:
             bool_str = bool_str.lower()
             if bool_str in ("true", "1"):
                 return True
@@ -279,7 +279,7 @@ class Command(JSONLike):
                     f"{self.stderr if stderr else self.stdout!r}."
                 )
 
-        parse_types = {
+        parse_types: dict[str, Callable[[str], Any]] = {
             "str": str,
             "int": int,
             "float": float,
@@ -296,31 +296,30 @@ class Command(JSONLike):
         )
         pattern = pattern.format(types_pattern=types_pattern, name=out_name)
         spec = self.stderr if stderr else self.stdout
+        assert spec is not None
         self.app.submission_logger.info(
             f"processing shell standard stream according to spec: {spec!r}"
         )
         param = self.app.Parameter(out_name)
         match = re.match(pattern, spec)
-        try:
-            groups = match.groups()
-        except AttributeError:
+        if match is None:
             return value
-        else:
-            parse_type, parse_args_str = groups[1:3]
-            parse_args = self._prepare_kwargs_from_string(
-                args_str=parse_args_str,
+        groups = match.groups()
+        parse_type, parse_args_str = groups[1:3]
+        parse_args = self._prepare_kwargs_from_string(
+            args_str=parse_args_str,
+            doubled_quoted_args=["delim"],
+        )
+        if param._value_class:
+            method, method_args_str = groups[3:5]
+            method_args = self._prepare_kwargs_from_string(
+                args_str=method_args_str,
                 doubled_quoted_args=["delim"],
             )
-            if param._value_class:
-                method, method_args_str = groups[3:5]
-                method_args = self._prepare_kwargs_from_string(
-                    args_str=method_args_str,
-                    doubled_quoted_args=["delim"],
-                )
-                method = method or "CLI_parse"
-                value = getattr(param._value_class, method)(value, **method_args)
-            if parse_type:
-                value = parse_types[parse_type](value, **parse_args)
+            method = method or "CLI_parse"
+            value = getattr(param._value_class, method)(value, **method_args)
+        if parse_type:
+            value = parse_types[parse_type](value, **parse_args)
 
         return value
 
