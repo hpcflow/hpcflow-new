14e7c77c26c4c4b6e52fc54dd31a80f9c473a6e6
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2024-08-13 10:01:13 +0100

    Em-dash

--- hpcflow/sdk/data/__init__.py
+++ hpcflow/sdk/data/__init__.py
@@ -3,11 +3,11 @@ YAML schemas.
 
 Contents:
 
-* ``config_file_schema.yaml`` &mdash; Schema for configuration selection files.
-* ``config_schema.yaml`` &mdash; Schema for configuration files.
-* ``environments_spec_schema.yaml`` &mdash; Schema for execution environment definition files.
-* ``files_spec_schema.yaml`` &mdash; Schema for input/output specification files.
-* ``parameters_spec_schema.yaml`` &mdash; Schema for parameter specification files.
-* ``task_schema_spec_schema.yaml`` &mdash; Schema for task template specification files.
-* ``workflow_spec_schema.yaml`` &mdash; Schema for workflow files.
+* ``config_file_schema.yaml`` – Schema for configuration selection files.
+* ``config_schema.yaml`` – Schema for configuration files.
+* ``environments_spec_schema.yaml`` – Schema for execution environment definition files.
+* ``files_spec_schema.yaml`` – Schema for input/output specification files.
+* ``parameters_spec_schema.yaml`` – Schema for parameter specification files.
+* ``task_schema_spec_schema.yaml`` – Schema for task template specification files.
+* ``workflow_spec_schema.yaml`` – Schema for workflow files.
 """
